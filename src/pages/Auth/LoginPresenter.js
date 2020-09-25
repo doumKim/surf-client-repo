d904209d50de2f465653cc6e54888109d30bf59e
hankyeolk <2247han@gmail.com>
2020-09-25 17:28:24 +0900

    [Client]/#8/feature: mypage component structuring
    마이페이지 컴포넌트 작업
    이미지 업로드 구현

--- src/pages/Auth/LoginPresenter.js
+++ src/pages/Auth/LoginPresenter.js
@@ -117,8 +117,8 @@ const SocialButton = styled.button`
   display: block;
   // color-set
   background-color: ${props => {
-    const socials = ["kakao", "google", "facebook", "submit"];
-    const socialColors = ["#fcc419", "#fa5252", "#4A68Ad", "#ced4da"];
+    const socials = ["kakao", "google", "naver", "submit"];
+    const socialColors = ["#fcc419", "#fa5252", "#51cf66", "#ced4da"];
     return socialColors[socials.indexOf(props.social)];
   }};
   color: white;
@@ -264,7 +264,7 @@ export default function Login({ close, LoginUp, SocialLoginUp, changeForm }) {
             Kakao
           </SocialButton>
           <SocialButton social="google">Google</SocialButton>
-          <SocialButton social="facebook">Facebook</SocialButton>
+          <SocialButton social="naver">Naver</SocialButton>
         </SocialWrap>
         <GotoJoin>
           계정이 없으신가요? <span onClick={changeForm}>회원가입</span> 하러가기
